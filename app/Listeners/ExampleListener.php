f17932474658bddb01efdbf33ba2858ca84a60ca
Andrew Kovalyov <andrew.kovalyoff@gmail.com>
2016-07-01 10:13:47 +0300

    Fix fiels with symfony cs fixer level

--- app/Listeners/ExampleListener.php
+++ app/Listeners/ExampleListener.php
@@ -3,15 +3,11 @@
 namespace App\Listeners;
 
 use App\Events\ExampleEvent;
-use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Contracts\Queue\ShouldQueue;
 
 class ExampleListener
 {
     /**
      * Create the event listener.
-     *
-     * @return void
      */
     public function __construct()
     {
@@ -21,8 +17,7 @@ class ExampleListener
     /**
      * Handle the event.
      *
-     * @param  ExampleEvent  $event
-     * @return void
+     * @param ExampleEvent $event
      */
     public function handle(ExampleEvent $event)
     {
--- database/migrations/2016_05_28_100609_create_slices_table.php
+++ database/migrations/2016_05_28_100609_create_slices_table.php
@@ -7,8 +7,6 @@ class CreateSlicesTable extends Migration
 {
     /**
      * Run the migrations.
-     *
-     * @return void
      */
     public function up()
     {
@@ -22,8 +20,6 @@ class CreateSlicesTable extends Migration
 
     /**
      * Reverse the migrations.
-     *
-     * @return void
      */
     public function down()
     {
