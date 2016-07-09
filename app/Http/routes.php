eecc9309c71c8a69dc2036fee08408d1cfa1b669
Дмитрий Басавин <basavind@gmail.com>
2016-07-10 01:03:22 +0300

    style (Routes) group all routes by entities

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,20 +11,37 @@
 |
 */
 
-$app->get('material', 'MaterialController@index');
-$app->post('material', 'MaterialController@create');
-$app->get('material/{material}', 'MaterialController@show');
-$app->delete('material/{material}', 'MaterialController@destroy');
-$app->patch('material/{material}', 'MaterialController@update');
-
-$app->group(['prefix' => 'material/{material}', 'namespace' => 'App\Http\Controllers'], function () use ($app) {
-    $app->get('slice', 'SliceController@index');
-    $app->post('slice', 'SliceController@create');
-    $app->get('slice/{slice_id}', 'SliceController@show');
-    $app->delete('slice/{slice}', 'SliceController@destroy');
-    $app->patch('slice/{slice}', 'SliceController@update');
+/*
+|--------------------------------------------------------------------------
+| Material Routes
+|--------------------------------------------------------------------------
+*/
+$app->group(['prefix' => 'material', 'namespace' => 'App\Http\Controllers'], function () use ($app) {
+    $app->get('/', 'MaterialController@index');
+    $app->post('/', 'MaterialController@create');
+    $app->get('/{material}', 'MaterialController@show');
+    $app->delete('/{material}', 'MaterialController@destroy');
+    $app->patch('/{material}', 'MaterialController@update');
 });
 
+/*
+|--------------------------------------------------------------------------
+| Slice Routes
+|--------------------------------------------------------------------------
+*/
+$app->group(['prefix' => 'material/{material}/slice', 'namespace' => 'App\Http\Controllers'], function () use ($app) {
+    $app->get('/', 'SliceController@index');
+    $app->post('/', 'SliceController@create');
+    $app->get('/{slice}', 'SliceController@show');
+    $app->delete('/{slice}', 'SliceController@destroy');
+    $app->patch('/{slice}', 'SliceController@update');
+});
+
+/*
+|--------------------------------------------------------------------------
+| Tag Routes
+|--------------------------------------------------------------------------
+*/
 $app->group(['prefix' => 'tag', 'namespace' => 'App\Http\Controllers'], function () use ($app) {
     $app->get('/', 'TagController@index');
     $app->post('/', 'TagController@create');
